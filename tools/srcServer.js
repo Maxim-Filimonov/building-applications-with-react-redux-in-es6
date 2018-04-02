f1f232574bb04091c6243e455a7b1ea0000f46c7
Maxim Filimonov <tpaktopsp@gmail.com>
2018-04-02 11:38:36 +0800

    initial setup

---
+++ tools/srcServer.js
@@ -0,0 +1,32 @@
+import config from "../webpack.config.dev";
+import express from "express";
+import open from "open";
+import path from "path";
+import webpack from "webpack";
+
+/* eslint-disable no-console */
+
+const port = 3000;
+const app = express();
+const compiler = webpack(config);
+
+app.use(
+  require("webpack-dev-middleware")(compiler, {
+    noInfo: true,
+    publicPath: config.output.publicPath
+  })
+);
+
+app.use(require("webpack-hot-middleware")(compiler));
+
+app.get("*", function(req, res) {
+  res.sendFile(path.join(__dirname, "../src/index.html"));
+});
+
+app.listen(port, function(err) {
+  if (err) {
+    console.log(err);
+  } else {
+    open(`http://localhost:${port}`, "Opera");
+  }
+});
