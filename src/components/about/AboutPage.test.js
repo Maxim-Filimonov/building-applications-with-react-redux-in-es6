8ac8bdae97bf0bd8fb893b1e6a450e2355e6af8b
Maxim Filimonov <tpaktopsp@gmail.com>
2018-04-03 18:58:46 +0800

    About page

--- src/components/about/AboutPage.test.js
+++ src/components/about/AboutPage.test.js
@@ -1,8 +1,8 @@
 import AboutPage from "./AboutPage";
 import React from "react";
-import { shallow } from "enzyme";
+import { mount } from "enzyme";
 describe("AboutPage", () => {
   it("renders", () => {
-    shallow(<AboutPage />);
+    mount(<AboutPage />);
   });
 });
