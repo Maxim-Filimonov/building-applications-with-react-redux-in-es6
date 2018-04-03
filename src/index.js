7c22ce4a8dc082c9fe81ab9910a0e64ab01fede4
Maxim Filimonov <tpaktopsp@gmail.com>
2018-04-03 19:00:36 +0800

    use the routes

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
-// console.log("hi");
+import "babel-polyfill";
+
+import AboutPage from "./components/about/AboutPage";
+import App from "./components/App";
+import HomePage from "./components/home/HomePage";
+import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+import Routes from "./routes";
+import history from "history";
+import { render } from "react-dom";
+
+// import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+
+// import styles from "./styles/styles.css";
+
+render(
+  <Router>
+    <Routes />
+  </Router>,
+  document.getElementById("app")
+);
